f939f94c2ec13d2a2fd269287e9ea8ae8c2ecc95
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 13:53:15 +0300

    Created Role class and BaseEntity. Updated Company class.

---
+++ src/main/java/com/worktrack/worktrack/domain/entities/Company.java
@@ -0,0 +1,40 @@
+package com.worktrack.worktrack.domain.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "companies")
+public class Company extends BaseUserEntity {
+
+    private String companyName;
+    private JobArea jobArea;
+    private String logo;
+
+    @Column(name = "company_name",nullable = false)
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "job_area",nullable = false)
+    public JobArea getJobArea() {
+        return jobArea;
+    }
+
+    public void setJobArea(JobArea jobArea) {
+        this.jobArea = jobArea;
+    }
+
+    @Column(name = "logo",nullable = false)
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
