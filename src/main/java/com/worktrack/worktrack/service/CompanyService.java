d93d2c45365bae3f82da3f5f66d981e43c705c0e
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 14:41:38 +0300

    Added two methods to RoleService - seedRolesInDb and assignCompanyRoles.

--- src/main/java/com/worktrack/worktrack/service/CompanyService.java
+++ src/main/java/com/worktrack/worktrack/service/CompanyService.java
@@ -3,4 +3,6 @@ package com.worktrack.worktrack.service;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface CompanyService extends UserDetailsService {
+
+
 }
--- src/main/java/com/worktrack/worktrack/service/RoleService.java
+++ src/main/java/com/worktrack/worktrack/service/RoleService.java
@@ -1,4 +1,10 @@
 package com.worktrack.worktrack.service;
 
+import com.worktrack.worktrack.domain.models.service.CompanyServiceModel;
+
 public interface RoleService {
+
+    void seedRolesInDb();
+
+    void assignCompanyRoles(CompanyServiceModel companyServiceModel);
 }
