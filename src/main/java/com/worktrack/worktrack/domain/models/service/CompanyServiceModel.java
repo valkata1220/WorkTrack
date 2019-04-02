0b3baf50263763e2ed7bf6b180579c14ae31e6eb
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 14:38:25 +0300

    Created CompanyServiceModel that extends base BaseServiceModel

---
+++ src/main/java/com/worktrack/worktrack/domain/models/service/CompanyServiceModel.java
@@ -0,0 +1,81 @@
+package com.worktrack.worktrack.domain.models.service;
+
+import com.worktrack.worktrack.domain.entities.JobArea;
+
+import java.util.Set;
+
+public class CompanyServiceModel extends BaseServiceModel {
+
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String companyName;
+    private String jobArea;
+    private String logo;
+    private Set<RoleServiceModel> authorities;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getJobArea() {
+        return jobArea;
+    }
+
+    public void setJobArea(String jobArea) {
+        this.jobArea = jobArea;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Set<RoleServiceModel> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<RoleServiceModel> authorities) {
+        this.authorities = authorities;
+    }
+}
