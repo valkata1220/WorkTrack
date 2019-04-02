3f9b8ae5b4e5ed06f810c47353c55c874e1eb4f9
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 14:30:42 +0300

    Created BaseServiceModel with id field.

---
+++ src/main/java/com/worktrack/worktrack/domain/models/service/BaseServiceModel.java
@@ -0,0 +1,14 @@
+package com.worktrack.worktrack.domain.models.service;
+
+public class BaseServiceModel {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
