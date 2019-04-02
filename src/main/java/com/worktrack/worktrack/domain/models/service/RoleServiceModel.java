654754c9bf9b718fb28c024993449153159446c0
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 14:35:27 +0300

    Created RoleServiceModel that extends base ServiceModel

---
+++ src/main/java/com/worktrack/worktrack/domain/models/service/RoleServiceModel.java
@@ -0,0 +1,14 @@
+package com.worktrack.worktrack.domain.models.service;
+
+public class RoleServiceModel extends BaseServiceModel {
+
+    private String authority;
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
