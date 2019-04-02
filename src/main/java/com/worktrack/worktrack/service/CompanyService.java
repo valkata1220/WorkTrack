2754e573d3069f85b754b85b6dc7a5101058eff3
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 14:42:40 +0300

    Added one method to CompanyService - registerCompany.

--- src/main/java/com/worktrack/worktrack/service/CompanyService.java
+++ src/main/java/com/worktrack/worktrack/service/CompanyService.java
@@ -1,8 +1,9 @@
 package com.worktrack.worktrack.service;
 
+import com.worktrack.worktrack.domain.models.service.CompanyServiceModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface CompanyService extends UserDetailsService {
 
-
+    CompanyServiceModel registerCompany(CompanyServiceModel companyServiceModel);
 }
