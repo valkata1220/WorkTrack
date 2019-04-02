89614af8242b25699d600c37b9117f18cea8e8d7
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-02 13:50:43 +0300

    Creating BaseEntity with id field

---
+++ src/main/java/com/worktrack/worktrack/domain/entities/BaseUserEntity.java
@@ -0,0 +1,100 @@
+package com.worktrack.worktrack.domain.entities;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class BaseUserEntity extends BaseEntity implements UserDetails {
+
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private Set<Role> authorities;
+
+    @Column(name = "username", nullable = false, unique = true, updatable = false)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "password",nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "email",nullable = false,unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "phone_number",nullable = false)
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    @ManyToMany(targetEntity = Role.class,fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id",
+                    referencedColumnName = "id"
+            )
+    )
+    public Set<Role> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
--- src/main/java/com/worktrack/worktrack/domain/entities/Worker.java
+++ src/main/java/com/worktrack/worktrack/domain/entities/Worker.java
@@ -1,8 +1,51 @@
 package com.worktrack.worktrack.domain.entities;
 
-public class Worker extends BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "workers")
+public class Worker extends BaseUserEntity {
 
     private String firstName;
     private String lastName;
-    private String email;
+    private Integer age;
+    private String nationality;
+
+    @Column(name = "first_name",nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name",nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "age",nullable = false)
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Column(name = "nationality",nullable = false)
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }
