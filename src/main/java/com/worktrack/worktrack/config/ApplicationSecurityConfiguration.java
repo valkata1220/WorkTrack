aaba5974b9e7316087609cfc35f4ec6853451e7c
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-08 21:41:11 +0300

    Created BaseController with three methods. Created Application Security Configuration and updated HomeController.

---
+++ src/main/java/com/worktrack/worktrack/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.worktrack.worktrack.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/css/**","/img/**","/js/**","/video/**").permitAll()
+                .antMatchers("/","company-index","worker-index").anonymous()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/company/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/company/home")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+
+    }
+}
--- src/main/java/com/worktrack/worktrack/web/controllers/HomeController.java
+++ src/main/java/com/worktrack/worktrack/web/controllers/HomeController.java
@@ -1,19 +1,29 @@
 package com.worktrack.worktrack.web.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class HomeController extends BaseController{
+
+    @GetMapping("/")
+    @PreAuthorize("isAnonymous()")
+    public ModelAndView index(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 
     @GetMapping("/company-index")
+    @PreAuthorize("isAnonymous()")
     public ModelAndView company(ModelAndView modelAndView){
         modelAndView.setViewName("company-index");
         return modelAndView;
     }
 
     @GetMapping("/worker-index")
+    @PreAuthorize("isAnonymous()")
     public ModelAndView worker(ModelAndView modelAndView){
         modelAndView.setViewName("worker-index");
         return modelAndView;
