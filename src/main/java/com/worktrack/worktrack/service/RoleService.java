0fd8cc4c6db4c5aa4274325cf8ba060d993baf03
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-08 15:34:44 +0300

    Created RoleService interface

--- src/main/java/com/worktrack/worktrack/service/RoleService.java
+++ src/main/java/com/worktrack/worktrack/service/RoleService.java
@@ -1,10 +1,17 @@
 package com.worktrack.worktrack.service;
 
 import com.worktrack.worktrack.domain.models.service.CompanyServiceModel;
+import com.worktrack.worktrack.domain.models.service.RoleServiceModel;
+
+import java.util.Set;
 
 public interface RoleService {
 
     void seedRolesInDb();
 
-    void assignCompanyRoles(CompanyServiceModel companyServiceModel);
+//    void assignCompanyRoles(CompanyServiceModel companyServiceModel,long numberOfUsers);
+
+    Set<RoleServiceModel> findAllRoles();
+
+    RoleServiceModel findByAuthority(String authority);
 }
