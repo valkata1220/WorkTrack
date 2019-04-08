96607181d4dcdfb3d8f00196a078185bb293151f
Valentin Tsvetkov <valkata1220@gmail.com>
2019-04-08 17:56:09 +0300

    Added bean BCryptPasswordEncoder.

--- src/main/java/com/worktrack/worktrack/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/worktrack/worktrack/config/ApplicationBeanConfiguration.java
@@ -3,6 +3,7 @@ package com.worktrack.worktrack.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -11,4 +12,7 @@ public class ApplicationBeanConfiguration {
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder();}
 }
